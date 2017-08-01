cd7346f20b48a1b4e85bbe7c882c4a2f3276fe08
Vadim Sharai <dev.vadim.sharai@gmail.com>
2017-08-01 16:38:59 +0300

    V 1.1.2 Minor fix

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var passport = require('passport');
 
 var app = express();
 
-//app.use(bodyParser.urlencoded());
-app.use(methodOverride(function (req, res) {
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride((req, res) => {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._method;
+        let method = req.body._method;
         delete req.body._method;
         return method;
     }
@@ -37,7 +37,6 @@ app.use(session({
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
