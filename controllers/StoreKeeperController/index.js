bf3e883060e4bea7950691fe060f09428eabed62
Vlad Revenko <vladiknorik@gmail.com>
2017-08-02 12:34:09 +0300

    Mistakes correction

--- controllers/StoreKeeperController/index.js
+++ controllers/StoreKeeperController/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     if (req.session.user) {
-        res.render('roles/storekeeper');
+        res.render('roles/storekeeper', {typeUser: req.session.user});
     } else {
         res.redirect('/');
     }
