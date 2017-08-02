f109575f06f2ba4e6e4994d5ff8ca1389e58a394
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-08-02 13:44:05 +0300

    Improve redirects, using passport.js

--- controllers/IdentificationController/_checkIdentification.js
+++ controllers/IdentificationController/_checkIdentification.js
@@ -5,27 +5,22 @@ const roles = require("../../consts/roles");
 module.exports = function _checkIdentification(req, res, typeUserID) {
     switch (typeUserID) {
         case roles.ADMIN: {
-            req.session.user = roles.ADMIN;
             res.redirect('/admin');
             break;
         }
         case roles.MODERATOR: {
-            req.session.user = roles.MODERATOR;
             res.redirect('/moderator');
             break;
         }
         case roles.EXECUTOR: {
-            req.session.user = roles.EXECUTOR;
             res.redirect('/executor');
             break;
         }
         case roles.STOREKEEPER: {
-            req.session.user = roles.STOREKEEPER;
             res.redirect('/store-keeper');
             break;
         }
         case roles.CUSTOMER: {
-            req.session.user = roles.CUSTOMER;
             res.redirect('/customer');
             break;
         }
