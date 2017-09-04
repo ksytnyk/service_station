fcb0f618884136f1fd59be44ece6fb0f9c7bace4
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-09-04 20:57:57 +0300

    V 3.12.9 Optimize getHoldTasks function for admin/moder handler.

--- controllers/AdminModeratorController/index.js
+++ controllers/AdminModeratorController/index.js
@@ -101,52 +101,22 @@ router.get('/requests/:status', (req, res) => {
     Request
         .getAllRequests(findBy)
         .then(requests => {
-            if (req.params.status === 'hold') {
-                Task
-                    .getAllHoldTasks()
-                    .then(tasks => {
-                        User
-                            .getAllUsers()
-                            .then(users => {
-                                res.render('roles/admin_moderator/requests', {
-                                    assignedExecutorUsers: users,
-                                    requests: requestsFactory(requests, tasks, true),
-                                    typeUser: req.session.passport.user.userTypeID
-                                });
-                            })
-                            .catch(error => {
-                                console.warn(error);
-                                res.render('roles/admin_moderator/requests');
+            method(req.params.status)
+                .then(tasks => {
+                    User
+                        .getAllUsers()
+                        .then(users => {
+                            res.render('roles/admin_moderator/requests', {
+                                assignedExecutorUsers: users,
+                                requests: requestsFactory(requests, tasks, true),
+                                typeUser: req.session.passport.user.userTypeID
                             });
-                    })
-                    .catch(error => {
-                        console.warn(error);
-                        res.render('roles/admin_moderator/requests');
-                    });
-
-            } else {
-                Task
-                    .getAllTasks()
-                    .then(tasks => {
-                        User
-                            .getAllUsers()
-                            .then(users => {
-                                res.render('roles/admin_moderator/requests', {
-                                    assignedExecutorUsers: users,
-                                    requests: requestsFactory(requests, tasks),
-                                    typeUser: req.session.passport.user.userTypeID
-                                });
-                            })
-                            .catch(error => {
-                                console.warn(error);
-                                res.render('roles/admin_moderator/requests');
-                            });
-                    })
-                    .catch(error => {
-                        console.warn(error);
-                        res.render('roles/admin_moderator/requests');
-                    });
-            }
+                        })
+                        .catch(error => {
+                            console.warn(error);
+                            res.render('roles/admin_moderator/requests');
+                        });
+                });
         })
         .catch(error => {
             console.warn(error);
@@ -248,7 +218,7 @@ router.delete('/delete-request/:id', (req, res) => {
             Request
                 .deleteRequest(req.params.id)
                 .then(() => {
-                    res.status(200).send({result:'ok'});
+                    res.status(200).send({result: 'ok'});
                 })
                 .catch(error => {
                     console.warn(error);
@@ -577,5 +547,24 @@ router.post('/create-global-request', validation.createGlobalRequest(), (req, re
             res.status(400).send({errors: errors});
         });
 });
+
+function method(isHold) {
+    return new Promise(function (resolve, reject) {
+
+        if (isHold === 'hold') {
+            Task
+                .getAllHoldTasks()
+                .then(result => {
+                    resolve(result);
+                })
+        } else {
+            Task
+                .getAllTasks()
+                .then(result => {
+                    resolve(result);
+                })
+        }
+    })
+}
 
 module.exports = router;
