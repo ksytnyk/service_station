fcdb668dafcaa44a01ff85cc525f8444d55ad9e0
Vlad Revenko <vladiknorik@gmail.com>
2018-05-08 17:56:57 +0300

    Turned off sequelize logging.

--- models/connection/index.js
+++ models/connection/index.js
@@ -6,6 +6,7 @@ var config = require('../../config/default.json');
 module.exports = new Sequelize(config.postgreSQL.database, config.postgreSQL.username, config.postgreSQL.password, {
     host: config.postgreSQL.host,
     dialect: config.postgreSQL.dialect,
+    logging: false,
     pool: {
         max: config.postgreSQL.pool.max,
         min: config.postgreSQL.pool.min,
